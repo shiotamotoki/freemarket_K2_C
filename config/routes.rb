a53ff03a49d5d2743856fb8515852f7a3742a61b
shiotamotoki <shiota1974@gmail.com>
2019-10-05 17:07:01 +0900

    add-mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'ok#index'
   get 'ok/index'
+  get 'mypages' => 'mypages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
