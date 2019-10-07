4b3f568916584db819ed26deb41cc319f2438cb5
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2019-10-08 00:07:21 +0900

    テーブル作成における軽微な修正
    ・ユーザーモデル
    ・クレジットカードテーブル作成のマイグレーションファイル
    上記に不正なSPがあり修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   has_one :personal_information, dependent: :destroy
   accepts_nested_attributes_for :personal_information
   has_many :items
+  has_many :credit_cards, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_one :user_evaluation, dependent: :destroy
+  has_many :buyers, dependent: :destroy
 
-
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/migrate/20191007134512_create_credit_cards.rb
+++ db/migrate/20191007134512_create_credit_cards.rb
@@ -1,7 +1,7 @@
 class CreateCreditCards < ActiveRecord::Migration[5.2]
   def change
     create_table :credit_cards do |t|
-      t.references   :user,                          null: false, foeign_key:true
+      t.references   :user,                          null: false, foeign_key: true
       t.integer      :number,                        null: false, unique: true
       t.integer      :month,                         null: false
       t.integer      :year,                          null: false
