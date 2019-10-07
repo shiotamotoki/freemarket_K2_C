35ed4e538f0a4497a8c76bee3a547f3daa983c8f
KATAOKA TAICHI <komaki.1027.math@gmail.com>
2019-10-07 23:05:41 +0900

    トップページ周り修正
    ・コントローラー
    ・ルート

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   root to: 'ok#index'
   get 'ok/index'
   get 'mypages' => 'mypages#index'
-  get 'toppage' => 'toppage#main'
+  get 'toppage' => 'toppage#index'
   get 'itemsshow' => 'items#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
