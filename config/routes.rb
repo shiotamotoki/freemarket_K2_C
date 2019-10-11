32b22ab2389d0fb23a906a394676a548831a6956
dai000 <four6ezweb.ne.jp>
2019-10-11 15:59:59 +0900

    ログアウト画面２

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,6 @@ Rails.application.routes.draw do
   get 'toppage' => 'toppage#main'
   get 'itemsshow' => 'items#show'
   get 'users/logout' => 'users#logout'
-  get 'users/logout' => 'users#logout'
   get 'users/credit' => 'users#credit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
