366803f943f88b974c85f16bd4e1bf163f27abc0
ononsutead <ononsutead@gmail.com>
2019-10-18 21:01:51 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.all.limit(10)
+    @items = Item.all
     @users = User.all
+    @item_images = ItemImage.all
+    @user_evaluations = UserEvaluation.all
+    @brands = Brand.all
+    @categories = Category.all
+    @personal_informations = PersonalInformation.all
+    binding.pry
   end
 end
