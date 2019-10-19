faa172c712f9c11b5d52abee815f0d498ccc6913
shiotamotoki <shiota1974@gmail.com>
2019-10-19 12:46:11 +0900

    shippingdate修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
-  belongs_to_active_hash :shippingdate
+  belongs_to_active_hash :shipping_date
   belongs_to_active_hash :clothingsize
   belongs_to_active_hash :shoessize
 end
