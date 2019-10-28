50fa1a467742b5d286ab7bdd147db26d22f6e488
dai000 <four6ezweb.ne.jp>
2019-10-28 12:41:58 +0900

    items-delete2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController 
   before_action :authenticate_user!, only: [:sell, :create, :edit, :update, :destroy, :buy]
+  before_action :set_item, only: [:show, :edit]
 
   def index
     @items = Item.all.limit(10)
@@ -7,7 +8,7 @@ class ItemsController < ApplicationController
 
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     @items = Item.all
     @users = User.all
     @item_images = ItemImage.all
@@ -21,16 +22,40 @@ class ItemsController < ApplicationController
     @same_items = Item.where( [ "id != ? and user_id != ?", params[:id], @item.user_id ] ).where(brand_id: @item.brand_id ).order("created_at DESC").limit(6)
   end
 
+  # def destroy
+  #  item = Item.find(params[:id])
+  #   if item.user_id == current_user.id
+  #   item.destroy
+  #   redirect_to root_path
+  #   end
+  # end
+
   def destroy
-   item = Item.find(params[:id])
+    # binding.pry
+    item = Item.find(params[:id])
     if item.user_id == current_user.id
-    item.destroy
-    redirect_to root_path
+     item.destroy
+     redirect_to root_path and return
+    else
+    #  flash[:alert] = '削除出来ませんでした'
+    #  render("item/edit")
+    flash[:alert] = '削除出来ませんでした'
+     redirect_to action: 'edit' 
     end
-  end
+
+   end
+
+
 
   def edit
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
 
+  end
+
+
+private 
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
