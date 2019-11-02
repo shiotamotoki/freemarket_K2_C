a799eba08d0cfa4eb15eb9d8d493032fad5ea8a9
ononsutead <ononsutead@gmail.com>
2019-11-02 09:48:10 +0900

    a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,14 +9,7 @@ class ItemsController < ApplicationController
 
   def show
     @items = Item.all
-    @users = User.all
     @item_images = ItemImage.all
-    @user_evaluations = UserEvaluation.all
-    @brands = Brand.all
-    @categories = Category.all
-    @personal_informations = PersonalInformation.all
-    @brands = Brand.all
-    @shipping_date = ShippingDate.all
     @other_items = Item.where( [ "id != ? and user_id = ?", params[:id], @item.user_id ] ).order("created_at DESC").limit(6)
     @same_items = Item.where( [ "id != ? and user_id != ?", params[:id], @item.user_id ] ).where(brand_id: @item.brand_id ).order("created_at DESC").limit(6)
   end
