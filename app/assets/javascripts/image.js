dc2e69b3dd3258da2f16f602d761c0456a0744c1
yuga190378 <gayokunomiko@gmail.com>
2019-11-09 12:21:12 +0900

    エラー処理ok

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -9,35 +9,56 @@ $(document).on('turbolinks:load', function() {
           <a class="main-content__item__body__image-upload__clearfix__container__images__ul__image__btn__edit">編集</a>
           <a class="main-content__item__body__image-upload__clearfix__container__images__ul__image__btn__delete">削除</a>
         </div>
-      </li>`
+      </li>`;
     return html;
   }
-
+  function buildUL(){
+    var html =
+      `<div class="main-content__item__body__image-upload__clearfix__container__images have-item-0" data-preview="2">
+        <ul class="main-content__item__body__image-upload__clearfix__container__images__ul"></ul>
+      </div>`;
+    return html;
+  }
+  var preview = 1;
   $('#item_file-upload').change(function(){
     // console.log($(this).prop('name'));
     // console.log($(this).prop('files'));
-    $('.main-content__item__body__image-upload__error-text').remove();
+    $('.main-content__item__body__image-upload__error-text').children().remove();
     var file = $(this).prop('files');
+    var have = $(".main-content__item__body__image-upload__clearfix__dropbox").prop('class').replace(/[^0-9]/g, '');
+    //アップロードされる画像に異常があるか(枚数や拡張子)
+    var errorFlg = false;
+    if ((preview == 2 && (have+file.length) > 5) || (preview == 1 && (have+file.length) > 10)) {
+      var errorText = `<li>アップロードできる画像は10枚までです</li>`;
+      $('.main-content__item__body__image-upload__error-text').append(errorText);
+      errorFlg = true;
+    }
     for (var i = 0; i < file.length; i++){
       if(!file[i].type.match('image.jpg|image.jpeg|image.png')){
-        var errorText = `<ul class="main-content__item__body__image-upload__error-text">
-                          <li>ファイル形式はjpeg、またはpngが使用できます</li>
-                        </ul>`;
-        $('.main-content__item__body__image-upload').append(errorText);
-        return;
+        var errorText = `<li>ファイル形式はjpeg、またはpngが使用できます</li>`;
+        $('.main-content__item__body__image-upload__error-text').append(errorText);
+        errorFlg = true;
+        break;
       }
     }
-    var have = $(".main-content__item__body__image-upload__clearfix__dropbox").prop('class').replace(/[^0-9]/g, '');
+    if (errorFlg) {
+      return;
+    }
     for (var i = 0; i < file.length; i++){
+      if (have == 5) {
+        preview = 2;//haveが5になったらプレビューを2段目に
+        have = 0;
+        var viewUl = buildUL;
+        $('.main-content__item__body__image-upload__clearfix__container').append(viewUl);
+      }
       var reader = new FileReader()
       reader.onload = function(e){
         var imageView = buildHTML(e.target.result);
-        $('.main-content__item__body__image-upload__clearfix__container__images__ul').append(imageView);
+        $(`.main-content__item__body__image-upload__clearfix__container__images[data-preview="${preview}"] .main-content__item__body__image-upload__clearfix__container__images__ul`).append(imageView);
       }
       reader.readAsDataURL(file[i]);
       have++;
     }
-    
     $(".main-content__item__body__image-upload__clearfix__container__images, .main-content__item__body__image-upload__clearfix__dropbox").removeClass(function(index, className){
       var matchClass = className.match(/\bhave-item-\d/g);
       if(matchClass == null){
