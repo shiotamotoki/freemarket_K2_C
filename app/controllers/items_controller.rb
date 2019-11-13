f8dcadf94a93a1e041d56ed8712abb334946e40a
ononsutead <ononsutead@gmail.com>
2019-11-13 19:32:18 +0900

    ok

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
     @category_man = Item.where(category_id: 200..344)
     @category_electrical_appliances = Item.where(category_id: 893..978)
     @category_toy = Item.where(category_id: 680..792)
-  
+
   end
 
 
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
     @item_images = ItemImage.all
     @other_items = Item.where( [ "id != ? and user_id = ?", params[:id], @item.user_id ] ).order("created_at DESC").limit(6)
     @same_items = Item.where( [ "id != ? and user_id != ?", params[:id], @item.user_id ] ).where(brand_id: @item.brand_id ).order("created_at DESC").limit(6)
-    
+   
   end
 
 
